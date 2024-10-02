514369ca01928050af97e43a740b0cc0359551aa
lauta-dev <altairdiaz123@gmail.com>
2024-10-01 22:03:41 -0300

    add style

--- packages/client/script.js
+++ packages/client/script.js
@@ -1,7 +1,17 @@
-const box = document.querySelector(".app");
+const box = document.querySelector(".output");
 const input = document.querySelector("form");
 const loading = document.getElementById("loading");
 
+/** @type {HTMLButtonElement} */
+const buttonCopy = document.querySelector(".output_button_hide");
+
+/**
+ * @type {HTMLParagraphElement}  // Describe el tipo de elemento
+ * @description Elemento de párrafo que muestra un mensaje de éxito en la interfaz.
+ */
+const successMessage = document.querySelector(".successMessage");
+
+/** @param {string} url */
 async function fetchUrl(url) {
 	loading.style.display = "flex"; // Para mostrar un loading al enviar la petición
 
@@ -27,5 +37,32 @@ input.addEventListener("submit", async (event) => {
 
 	let data = await fetchUrl(url);
 
-	box.innerHTML = `<code>${data.short}</code>`;
+	box.textContent = data.short;
+
+	// Set output style
+	box.classList.remove("output");
+	box.classList.add("output_success");
+
+	// Set button style
+	buttonCopy.classList.remove("output_button_hide");
+	buttonCopy.classList.add("output_button");
 });
+
+// Copiar URL al portapapeles
+
+function copyTextToClipboard() {
+	const content = box.textContent;
+
+	successMessage.classList.remove("copy_disable");
+	successMessage.classList.add("copy_success");
+
+	setTimeout(() => {
+		successMessage.classList.remove("copy_success");
+		successMessage.classList.add("copy_disable");
+	}, 5000);
+
+	navigator.clipboard.writeText(content);
+}
+
+box.addEventListener("click", () => copyTextToClipboard());
+buttonCopy.addEventListener("click", () => copyTextToClipboard());
