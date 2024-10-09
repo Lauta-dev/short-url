18b7d5a70ec3e0a6619ea6c5ef69ae8ea1042461
lauta-dev <altairdiaz123@gmail.com>
2024-10-09 16:34:34 -0300

    add html code

--- packages/server/index.js
+++ packages/server/index.js
@@ -13,7 +13,6 @@ app.use(cors());
 app.post("/url/", async (req, res) => {
 	/** @type {string} */
 	const url = req.body.url;
-	console.log(req.body);
 
 	if (!url.startsWith("https://")) {
 		return res.status(400).json({
@@ -47,7 +46,56 @@ app.get("/short/:id", async (req, res) => {
 		});
 	}
 
-	res.redirect(url);
+	const html = `
+    <head>
+      <style>
+        * {
+          font-family:
+            -apple-system,
+            BlinkMacSystemFont,
+            'Segoe UI',
+            Roboto,
+            Oxygen,
+            Ubuntu,
+            Cantarell,
+            'Open Sans',
+            'Helvetica Neue',
+            sans-serif;
+        }
+      </style>
+      <title>Redirect to: ${url}</title>
+    </head>
+
+    <h2>
+        Redirect to:
+        <a href="${url}">${url}</a>
+        <b id="delay"></b>
+    </h2>
+		<script>
+      const limit = 5000
+      const afterXSeconds = (second) =>
+        \`after \${second}\ seconds\`
+
+      let delay = 5
+
+      setInterval(() => {
+        if (delay > -1) {
+          document
+            .getElementById("delay")
+            .textContent = afterXSeconds(delay)
+          delay--;
+      }
+      }, 1000);
+
+			setTimeout(() => {
+				window.location.href = "${url}";
+			}, limit);
+		</script>
+	`;
+
+	res.send(html);
 });
 
-app.listen(port);
+app.listen(port, () => {
+	console.log(`Server listening in port: ${port}`);
+});
