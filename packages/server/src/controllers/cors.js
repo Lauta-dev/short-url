588f057d2361778d049adeb965c5391b2fd73961
lauta-dev <altairdiaz123@gmail.com>
2024-10-11 22:03:59 -0300

    +

--- packages/server/src/controllers/cors.js
+++ packages/server/src/controllers/cors.js
@@ -1,5 +1,2 @@
-var whitelist = ["https://short-url-clien.vercel.app"];
 export const corsOptions = {
-	origin: whitelist[0],
-	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
