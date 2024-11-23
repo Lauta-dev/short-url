edaa82daa5fda9d272cf8d8db70b49873b7a90b0
lauta-dev <altairdiaz245@gmail.com>
2024-11-22 22:15:28 -0300

    final structure

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,24 +1,13 @@
-import { Button } from "@/components/ui/button";
 import Header from "@/components/me/header";
-import MenuMobile from "@/components/me/menuMobile";
-import useResizeWindow from "@/hook/useResizeWindow";
 import FormAction from "@/components/me/Form";
+import Footer from "@/components/me/Footer";
 
 function App() {
-	const { isMobile } = useResizeWindow();
-
 	return (
 		<main className="p-4">
-			{isMobile ? <MenuMobile /> : <Header />}
+			<Header />
 			<FormAction />
-
-			<div className="mt-4 p-3 text-center flex flex-col gap-4">
-				<p>URL:</p>
-				<b onClick={() => console.log("asd")}>https://google.com</b>
-				<Button variant="secondary">Copiar</Button>
-
-				<b className="text-green-600">URL Copiada</b>
-			</div>
+			<Footer />
 		</main>
 	);
 }
