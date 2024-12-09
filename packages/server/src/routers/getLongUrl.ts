36d8c6876099400c4aefc8254b7d0e4988340404
lauta-dev <altairdiaz245@gmail.com>
2024-12-09 13:13:00 -0300

    feat: return a html file if url is not valid

--- packages/server/src/routers/getLongUrl.ts
+++ packages/server/src/routers/getLongUrl.ts
@@ -1,20 +1,18 @@
 import express, { Response, Request } from "express";
 import getUrlById from "../db/getUrlById";
 import goTo from "@/goTo";
+import path from "node:path";
 
 const router = express.Router();
 
 router.get("/api/:id", async (req: Request<{ id: string }>, res: Response) => {
 	const { id } = req.params;
-	const { code, url, error, errorType } = await getUrlById({ id });
+	const { code, url, error } = await getUrlById({ id });
 
 	if (error) {
-		res.status(code).json({
-			code,
-			error,
-			url,
-			errorType,
-		});
+		return res
+			.status(code)
+			.sendFile(path.resolve(__dirname, "../src/expired.html"));
 	}
 
 	res.send(goTo(url));
