7f37f76971a0875e97b621476ea3fd4f2e117715
lauta-dev <altairdiaz245@gmail.com>
2024-12-09 22:26:53 -0300

    esbuild configuration

---
+++ packages/server/esbuild.config.cjs
@@ -0,0 +1,13 @@
+const esbuild = require('esbuild')
+const depedencies = Object.keys(require('./package.json').dependencies)
+
+esbuild.build({
+		entryPoints: ["index.ts"],
+		bundle: true,
+		outfile: "dist/bundle.cjs",
+		minify: true,
+		platform: "node",
+		format: "cjs",
+		external: depedencies,
+		treeShaking: true,
+	}).then(e => console.log("✅ Bundled successfully.")).catch(error => console.error(error));
