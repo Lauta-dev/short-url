b0719d88d260d59af66bec041f819f9cced6987b
lauta-dev <altairdiaz245@gmail.com>
2025-01-02 21:09:36 -0300

    feat: verify if token exists and save user id in db with the url

--- packages/server/src/db/PushUrl.ts
+++ packages/server/src/db/PushUrl.ts
@@ -1,17 +1,36 @@
 import turso from "@/db/turso";
+import { getInfoFromToken } from "@utils/getInfoFromToken";
+import { getUserById } from "@db/getUserById";
 
 interface Client {
 	url: string;
 	hostname: string;
 	expiredDate: string;
 	intentos: number;
+	token: string;
 }
 
-async function PushUrls({ url, hostname, expiredDate, intentos }: Client) {
+async function PushUrls({
+	url,
+	hostname,
+	expiredDate,
+	intentos,
+	token,
+}: Client) {
 	const query =
-		"INSERT INTO short_url (uuid, url, shor_url, valid_until_to, intentos) VALUES (?, ?, ?, ?, ?)";
+		"INSERT INTO short_url (uuid, url, shor_url, valid_until_to, intentos, user_id) VALUES (?, ?, ?, ?, ?, ?)";
 	const id = crypto.randomUUID().split("-")[0];
 	const short = `${hostname}/api/${id}`;
+	let userId;
+
+	if (token) {
+		const pay = getInfoFromToken(token);
+		const user = await getUserById(pay);
+
+		if (user?.exist) {
+			userId = user.id;
+		}
+	}
 
 	if (intentos < 0) {
 		intentos = 25;
@@ -19,7 +38,7 @@ async function PushUrls({ url, hostname, expiredDate, intentos }: Client) {
 
 	await turso.execute({
 		sql: query,
-		args: [id, url, short, expiredDate, intentos.toString()],
+		args: [id, url, short, expiredDate, intentos.toString(), userId || null],
 	});
 
 	return short;
