82dc6914a9b48fa32e47d4d894474b9997815f6b
lauta-dev <altairdiaz245@gmail.com>
2025-06-27 14:46:30 -0300

    asd

--- packages/server/config/cors.ts
+++ packages/server/config/cors.ts
@@ -1,7 +1,7 @@
 import { CorsOptions } from "cors";
 
 export const corsOptions: CorsOptions = {
-	origin: ["http://localhost:8080", "https://short-url-clien.vercel.app/"],
+	origin: ["http://localhost:8080", "https://short-url-clien.vercel.app", "https://short-url-clien.vercel.app/"],
 	credentials: true,
 	methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
 	allowedHeaders: ["Content-Type", "Authorization"],
